b862b21a1f02f9c04bc7a2bced943ef66c23fd66
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-26 02:15:30 +0600

    write-actions-in-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useDispatch } from "react-redux"
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 
 
 function App() {
 
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment())
+  }
+  const handleDecrement = () => {
+    dispatch(decrement())
+  }
+
   return (
     <>
-      
+
       <h1 className="text-blue-600 font-bold text-4xl">Counter</h1>
 
-      <button className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment</button>
-      <button className="px-3 py-1 bg-gray-100 rounded mr-2 ">+</button>
-      <button className="px-3 py-1 bg-gray-300 rounded ">Decrement</button>
-      
+      <button onClick={handleIncrement} className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment</button>
+      <button className="px-3 py-1 bg-gray-100 rounded mr-2 ">0</button>
+      <button onClick={handleDecrement} className="px-3 py-1 bg-gray-300 rounded ">Decrement</button>
+
     </>
   )
 }
