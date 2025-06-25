6110f9298ced8735bad753d9be893895f7c94433
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-26 01:29:23 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home';
+import Layout from './components/Layout';
+import About from './components/About';
+import Contact from './components/Contact';
+import ErrorPage from './components/ErrorPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
