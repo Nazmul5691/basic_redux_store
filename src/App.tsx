3d46f9e4757b61f80632866b67235e4f47304e01
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-26 22:13:25 +0600

    add data with action payload

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,26 @@ function App() {
 
   const dispatch = useAppDispatch();
   // const {count} = useSelector((state : RootState) => state.counter)
-  const {count} = useAppSelector((state) => state.counter)
+  const { count } = useAppSelector((state) => state.counter)
 
 
-  const handleIncrement = () => {
-    dispatch(increment())
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
   }
-  const handleDecrement = () => {
-    dispatch(decrement())
+  const handleDecrement = (amount: number) => {
+    dispatch(decrement(amount));
   }
 
   return (
     <>
       <h1 className="text-blue-600 font-bold text-4xl">Counter</h1>
 
-      <button onClick={handleIncrement} className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment</button>
+      <button onClick={() => handleIncrement(5)} className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment by 5</button>
+      <button onClick={() => handleIncrement(1)} className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment 1</button>
       <button className="px-3 py-1 bg-gray-100 rounded mr-2 ">{count}</button>
-      <button onClick={handleDecrement} className="px-3 py-1 bg-gray-300 rounded ">Decrement</button>
+      <button onClick={() => handleDecrement(5)} className="px-3 py-1 bg-gray-300 rounded ">Decrement by 5</button>
+      <button onClick={() => handleDecrement(1)} className="px-3 py-1 bg-gray-300 rounded ">Decrement 1</button>
+      {/* <button onClick={() => dispatch(decrement(5))} className="px-3 py-1 bg-gray-300 rounded ">Decrement 5</button> */}
 
     </>
   )
