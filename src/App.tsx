b81b05725b00259e67b28b4956486ac865435776
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-26 21:28:12 +0600

    use-state-and-fix-typescript-errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useDispatch } from "react-redux"
+
 import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 
 
 function App() {
 
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  // const {count} = useSelector((state : RootState) => state.counter)
+  const {count} = useAppSelector((state) => state.counter)
+
 
   const handleIncrement = () => {
     dispatch(increment())
@@ -16,11 +20,10 @@ function App() {
 
   return (
     <>
-
       <h1 className="text-blue-600 font-bold text-4xl">Counter</h1>
 
       <button onClick={handleIncrement} className="px-3 py-1 bg-gray-300 rounded mr-2 mt-4">Increment</button>
-      <button className="px-3 py-1 bg-gray-100 rounded mr-2 ">0</button>
+      <button className="px-3 py-1 bg-gray-100 rounded mr-2 ">{count}</button>
       <button onClick={handleDecrement} className="px-3 py-1 bg-gray-300 rounded ">Decrement</button>
 
     </>
